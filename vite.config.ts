bbec51b62f6a8e05884d12e6992780877cb96aa1
shakua <2504619594@qq.com>
2025-11-19 21:42:31 +0800

    完善方法映射

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,11 @@ export default defineConfig({
         target:'http://localhost:3001',
         changeOrigin:true,
         secure:false
+      },
+      '/rag': {
+        target:'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
